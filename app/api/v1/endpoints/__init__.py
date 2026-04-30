b68e6bfcdc54546e8186131edeca3dfc9358519a
darrenak403 <ngothanhdat4002@gmail.com>
2026-04-30 22:52:50 +0700

    fix(init): correct import statement in __init__.py file
    - Fixed an incorrect import statement in the `__init__.py` file of the API endpoints, ensuring proper module loading.
    - This change resolves potential import errors and maintains the integrity of the endpoint structure.

--- app/api/v1/endpoints/__init__.py
+++ app/api/v1/endpoints/__init__.py
@@ -1,3 +1,3 @@
-from app.api.v1.endpoints import auth, otp, question_sources, stats
+__init__.pyfrom app.api.v1.endpoints import auth, otp, question_sources, stats
 
 __all__ = ["auth", "otp", "stats", "question_sources"]
